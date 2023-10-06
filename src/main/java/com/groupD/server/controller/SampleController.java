b6e40f02fce9cf79db4b2591527c84e47f02e973
Bae Hyeonseo <haulqogustj@naver.com>
2023-10-06 19:35:05 +0900

    ci/cd

---
+++ src/main/java/com/groupD/server/controller/SampleController.java
@@ -0,0 +1,10 @@
+package com.groupD.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RequestMapping("/api/sample")
+@RestController
+public class SampleController {
+}
