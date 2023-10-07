d6b46028fa86988d963f57416a5963e6839c554d
eckrin <bsh51515@naver.com>
2023-10-07 16:48:23 +0900

    fix: cors설정 정보 수정

--- src/main/java/com/groupD/server/config/WebMvcConfig.java
+++ src/main/java/com/groupD/server/config/WebMvcConfig.java
@@ -13,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://diamond-hackathon-kutisms.vercel.app")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
--- src/main/java/com/groupD/server/controller/SampleController.java
+++ src/main/java/com/groupD/server/controller/SampleController.java
@@ -3,8 +3,6 @@ package com.groupD.server.controller;
 import com.groupD.server.dto.SampleResponse;
 import com.groupD.server.service.SampleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //
 public class SampleController {
-private final SampleService sampleService;
+    private final SampleService sampleService;
 
     @GetMapping("/get")
-    public ResponseEntity<SampleResponse> geturl(@RequestParam long id) throws Exception {
-        return new ResponseEntity<>( sampleService.getUrl(id), HttpStatus.OK);
+    public SampleResponse geturl(@RequestParam long id) throws Exception {
+        return sampleService.getUrl(id);
     }
 }
