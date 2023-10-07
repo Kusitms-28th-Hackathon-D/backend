a9038a8f1978e71296287bdfe97f44a84fa74e2c
eckrin <bsh51515@naver.com>
2023-10-07 17:30:33 +0900

    fix: filterchain config / 삭제

--- src/main/java/com/groupD/server/config/SpringSecurityConfig.java
+++ src/main/java/com/groupD/server/config/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig {
                             CorsConfigurationSource source = request -> {
                                 // Cors 허용 패턴
                                 CorsConfiguration config = new CorsConfiguration();
-                                config.setAllowedOrigins(List.of("http://localhost:3000", "https://diamond-hackathon-kutisms.vercel.app/"));
+                                config.setAllowedOrigins(List.of("http://localhost:3000", "https://diamond-hackathon-kutisms.vercel.app"));
                                 config.setAllowedMethods(List.of("*"));
                                 config.setAllowCredentials(true);
 
