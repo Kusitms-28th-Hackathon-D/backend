9002fac636863cb85347f6900cf764b187ff99e9
eckrin <bsh51515@naver.com>
2023-10-08 04:35:05 +0900

    feat: s3 사진 업로드 추가

--- src/main/java/com/groupD/server/controller/S3Controller.java
+++ src/main/java/com/groupD/server/controller/S3Controller.java
@@ -1,6 +1,8 @@
 package com.groupD.server.controller;
 
 import com.groupD.server.domain.dto.SignUpRequestDto;
+import com.groupD.server.domain.entity.Member;
+import com.groupD.server.repository.MemberRepository;
 import com.groupD.server.security.Auth;
 import com.groupD.server.security.AuthInfo;
 import com.groupD.server.service.S3Uploader;
@@ -17,14 +19,22 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class S3Controller {
     private final S3Uploader s3Uploader;
-
+    private final MemberRepository memberRepository;
     @PostMapping("/image")
-    public ResponseEntity<SignUpRequestDto> updateUserImage(@Auth AuthInfo authInfo, @RequestPart("file") MultipartFile multipartFile) {
+    public ResponseEntity<Void> updateUserImage(@Auth AuthInfo authInfo, @RequestPart("file") MultipartFile multipartFile) {
         try {
-            s3Uploader.uploadFiles(multipartFile, "static");
+            Member member = memberRepository.findByEmail(authInfo.getEmail())
+                    .orElseThrow(() -> new RuntimeException("Member not found"));
+
+            String imageUrl = s3Uploader.uploadFiles(multipartFile, "static");
+
+            // 회원의 imageUrl 필드 업데이트
+            member.setImageUrl(imageUrl);
+            memberRepository.save(member);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
